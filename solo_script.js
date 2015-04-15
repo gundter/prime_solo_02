e10db80a4fb799328b96a88851327378eee2da8e
Terry Gunderson <tgunderson25@gmail.com>
2015-04-15 18:07:32 -0500

    Adding CSS code

--- solo_script.js
+++ solo_script.js
@@ -49,12 +49,12 @@ function calculateSTI(array){
     bonus = 0.13;
   }
 
-  newArray[1] = bonus;
+  newArray[1] = " " + bonus;
   newArray[2] = baseSalary * (1.0 + bonus);
   newArray[2] = newArray[2] * 100;
   newArray[2] = Math.round(newArray[2]);
-  newArray[2] = newArray[2] / 100;
-  newArray[3] = Math.round(baseSalary * bonus);
+  newArray[2] = " " + newArray[2] / 100;
+  newArray[3] = " " + Math.round(baseSalary * bonus);
   console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
   return newArray;
 }
